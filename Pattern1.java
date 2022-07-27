9f7f0ef5bae93c1314fd4d2de1c651c93adda554
AKSHAY <87317537+akshayhere3001@users.noreply.github.com>
2022-07-27 11:39:56 +0530

    Update Pattern1.java

--- Pattern1.java
+++ Pattern1.java
@@ -9,8 +9,10 @@ public class Pattern1{
 		 	   * * * * * 
 		 */
 		int i, j;
-		for(i = 1; i<=4; i++) {
-			for(j = 1; j<= 5; j++) {
+	  	int rows = 4;
+	  	int cols = 5;
+		for(i = 1; i<=rows; i++) {
+			for(j = 1; j<= cols; j++) {
 				System.out.print(" * ");			
 			}
 			System.out.println();
