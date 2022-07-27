9996665abfe24bbe097de1a5e52aaf16040a5f90
AKSHAY <87317537+akshayhere3001@users.noreply.github.com>
2022-07-27 19:47:17 +0530

    Update Pattern8.java

--- Pattern8.java
+++ Pattern8.java
@@ -17,7 +17,8 @@ public class Pattern8{
 		
 		for(int i = 1; i<= rows; i++) {
 			for(int j = 1; j<= i; j++) {
-				System.out.print(count++ + " ");
+				System.out.print(count + " ");
+				count++;
 			}
 			System.out.println();
 		}
