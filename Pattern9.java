923404c9b044bc662a649404267a8ae8da098995
AKSHAY <87317537+akshayhere3001@users.noreply.github.com>
2022-07-27 19:58:23 +0530

    Create Pattern9.java

---
+++ Pattern9.java
@@ -0,0 +1,30 @@
+public class Pattern9{
+  public static void main(String[] args){
+    
+    // Patter-9 -> 0-1 Triangle
+		/*
+		 
+		 	1
+		 	0 1
+		 	1 0 1
+		 	0 1 0 1
+		 	1 0 1 0 1
+		  
+		 */
+		
+		int rows = 5;
+		
+		for(int i = 1; i<= rows; i++) {
+			for(int j = 1; j<= i; j++) {
+				int sum = i + j;
+				if(sum % 2 == 0) {
+					System.out.print(1 + " ");
+				}else {
+					System.out.print(0 + " ");
+				}
+			}
+			System.out.println();
+		}
+    
+  }
+}
