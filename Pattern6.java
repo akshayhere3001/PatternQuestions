de457e6596c5f224fb96a7ef87054ffb020f8edb
AKSHAY <87317537+akshayhere3001@users.noreply.github.com>
2022-07-27 19:30:09 +0530

    Update Pattern6.java

--- Pattern6.java
+++ Pattern6.java
@@ -15,7 +15,7 @@ public class Pattern6{
 		
 		for(int i = 1; i<= rows; i++) {
 			for(int j = 1; j<= i; j++) {
-				System.out.print(" " + j + " ");
+				System.out.print(j + " ");
 			}
 			System.out.println();
 		}
